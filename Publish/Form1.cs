62d1270f84db76968494a725f0f1398b8a60bfc6
Cedric HY Wong <95323089+CedriW@users.noreply.github.com>
2023-08-17 15:30:05 +0000

    Add publish

---
+++ Publish/Form1.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Publish
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+
+                string message = textBox1.Text + "\r\n";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                label2.Text = "message sent";
+            }
+        }
+    }
+}
